3fa85513768673c43ed5152741bd143211f32bad
Reinaldo <mr.reinaldo.g@gmail.com>
2026-04-26 21:03:02 -0400

    Debug: Enabled error reporting and fixed paths

--- mercadopago/checkout.php
+++ mercadopago/checkout.php
@@ -1,6 +1,10 @@
 <?php
+// Activar errores para ver qué está pasando realmente
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../src/Services/MercadoPagoService.php';
 
 use App\Services\MercadoPagoService;
 
