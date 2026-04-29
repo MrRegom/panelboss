df24680caa448a1a1b96dca591299b9ba14da870
Reinaldo <mr.reinaldo.g@gmail.com>
2026-04-29 14:24:20 -0400

    Corrected project root structure for production server

--- mercadopago/mercadopago_temp/checkout.php
+++ mercadopago/mercadopago_temp/checkout.php
@@ -1,14 +1,11 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../src/Services/MercadoPagoService.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/Services/MercadoPagoService.php';
 
 use App\Services\MercadoPagoService;
 
 // Cargar .env manualmente si Dotenv falla
-if (file_exists(__DIR__ . '/../../.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-    $dotenv->load();
-} elseif (file_exists(__DIR__ . '/../.env')) {
+if (file_exists(__DIR__ . '/../.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }
--- mercadopago/mercadopago_temp/test10.php
+++ mercadopago/mercadopago_temp/test10.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../src/Services/MercadoPagoService.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/Services/MercadoPagoService.php';
 
 use App\Services\MercadoPagoService;
 
