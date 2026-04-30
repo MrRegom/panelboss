eb75eeb3a0e135c03187075b5a894715d90b5656
Reinaldo <mr.reinaldo.g@gmail.com>
2026-04-29 21:37:42 -0400

    fix: Corregido namespace Database en visor administrativo.

--- admin/catalogo.php
+++ admin/catalogo.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Config/Database.php';
 require_once __DIR__ . '/../src/Repositories/MasterProductRepository.php';
 
+use App\Config\Database;
 use App\Services\AuthService;
 use App\Repositories\MasterProductRepository;
 
